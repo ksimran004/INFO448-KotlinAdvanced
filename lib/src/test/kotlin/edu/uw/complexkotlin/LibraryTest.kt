2f5aed39e248b3e46a2994e374d3fcea0e106e19
ksimran004 <sk04@uw.edu>
2024-10-16 21:12:45 -0700

    Completed Kotlin advanced assignment

--- lib/src/test/kotlin/edu/uw/complexkotlin/LibraryTest.kt
+++ lib/src/test/kotlin/edu/uw/complexkotlin/LibraryTest.kt
@@ -9,52 +9,62 @@ class LibraryTest {
     @Test fun fizzbuzzToTwo() {
         assertEquals("", fizzbuzz(1..2))
     }
+
     @Test fun fizzbuzzToThree() {
         assertEquals("FIZZ", fizzbuzz(1..3))
     }
+
     @Test fun fizzbuzzToSix() {
         assertEquals("FIZZBUZZFIZZ", fizzbuzz(1..6))
     }
+
     @Test fun fizzbuzzToTwelve() {
         assertEquals("FIZZBUZZFIZZFIZZBUZZFIZZ", fizzbuzz(1..12))
     }
+
     @Test fun fizzbuzzToFifteen() {
         assertEquals("FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ", fizzbuzz(1..15))
     }
+
+
     // Fizzbuzz to 25 should equal "FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZFIZZFIZZBUZZ"
     // Fizzbuzz to 50 should equal "FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZFIZZBUZZ"
 
-    //@Test fun fizzbuzzgenToFifteen() {
-    //    val fb = fizzbuzzgen(mapOf(3 to "FIZZ", 5 to "BUZZ"))
-    //    assertEquals("FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ", fb(1..15))
-    //}
+    // @Test fun fizzbuzzgenToFifteen() {
+    //     val fb = fizzbuzzgen(mapOf(3 to "FIZZ", 5 to "BUZZ"))
+    //     assertEquals("FIZZBUZZFIZZFIZZBUZZFIZZFIZZBUZZ", fb(1..15))
+    // }
+
 
     @Test fun r1Test() {
         assertEquals(">>> FOO: {BAR}", r1())
     }
+
     @Test fun r2Test() {
         assertEquals(">>> FOO: {WOOGAWOOGAWOOGA}", r2())
     }
 
+    // Philosopher Enum Tests
     @Test fun philosopherTests() {
         var seneca = Philosopher.THINKING
-        assertEquals("Deep thoughts....", seneca.toString())
+        assertEquals("Deep thoughts....", seneca.toString())  // Four dots
 
         // Shift to talking
         seneca = seneca.signal()
         assertEquals("Allow me to suggest an idea...", seneca.toString())
 
-        // Shift to thinking
+        // Shift back to thinking
         seneca = seneca.signal()
-        assertEquals("Deep thoughts....", seneca.toString())
+        assertEquals("Deep thoughts....", seneca.toString())  // Four dots
 
-        // Shift back to talking
+        // Shift back to talking again
         seneca = seneca.signal()
         assertEquals("Allow me to suggest an idea...", seneca.toString())
     }
 
+    // Command Class Tests
     @Test fun commandTests() {
-        assertEquals("", Command("")(""))
-        assertEquals("> Hello!", Command("> ")("Hello!"))
+        assertEquals("", Command("")(""))  // Empty prompt and message
+        assertEquals("> Hello!", Command("> ")("Hello!"))  // Custom prompt and message
     }
 }
